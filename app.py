b7ee54d767b00d3951022095a54d933d58bc131c
Patrick Tan <patrick4499@gmail.com>
2019-05-28 00:14:18 -0700

    done with cogs 18 final for now

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from googleapiclient.discovery import build
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 import os
-from forms import SearchForm
+import requests
+import random
+from forms import SearchForm, RegistrationForm, LoginForm
+
 
 app = Flask(__name__)
 
@@ -9,21 +12,78 @@ SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
 api_key = 'AIzaSyD2tUZp7juaZ_8z1yo6p7BxftxEAdO4z_Y'
-youtube = build('youtube', 'v3', developerKey=api_key)
-req = youtube.search().list(q="Drake" , part="snippet", type="video", maxResults=1).execute()
+'''
+req = youtube.search().list(q="Drake" , part="snippet", type="video", maxResults=1)
+res = req.execute()
+'''
 
-video_id = req['items'][0]['id']['videoId']
+countries = ["Chinese", "Japanese", "turn_up","Mexican", "French", "Russian", "Korean", "yikes" , "Indian", "Spanish", "African", "Veggie Tales"]
 
-videoData = {
-   # 'artist' : self.request.get('artist'),
-    #'country' : self.request.get('country'),
-    'search_response' : req,
-    'vidId': video_id
-  #  'title':title
-}
+@app.route("/", methods=['GET', 'POST'])
+@app.route("/home", methods=['GET', 'POST'])
+def home():
+    form = SearchForm()
+    if form.validate_on_submit():
+      flash(f'Genre is {form.genre.data}!', 'success')
 
-@app.route("/")
-def hello():
-  form = SearchForm()
-  return render_template('home.html', title='main', form=form)
+      youtube = build('youtube', 'v3', developerKey=api_key)
 
+      song_genre = f'{form.genre.data}'
+      secure_random = random.SystemRandom()
+      country = secure_random.choice(countries)
+
+      print(country + " " + song_genre)
+      search_response = youtube.search().list(
+              q = song_genre + " in " + country,
+              part = "snippet",
+              type = "video",
+              order = "viewCount",
+              maxResults=1,
+            ).execute()
+
+      vid_id = search_response['items'][0]['id']['videoId']
+      title = search_response['items'][0]['snippet']['title']
+
+      results = {
+        'search_response' : search_response,
+        'vidId':vid_id,
+        'title':title,
+        'genre':song_genre,
+        'country':country,
+      }
+
+      if country == "Veggie Tales":
+        return redirect(url_for('result', song_genre="qbw4nsNpqzA", country="farmville", name="EASTER EGG"))
+      elif country == "yikes":
+          return redirect(url_for('result', song_genre="I1ZEf032A1w", country="just enjoy", name="EASTER EGG"))
+      elif country == "turn_up":
+          return redirect(url_for('result', song_genre="VWoIpDVkOH0", country="GET LOW", name="BUST IT DOWN"))
+      elif country in countries:
+        return redirect(url_for('result', song_genre=results["vidId"], country=country, name=title))
+    
+    return render_template('home.html', title='Home', form=form)
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('You have been logged in! :)', 'success')
+        return redirect(url_for('home'))
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route("/result/<string:song_genre>/<string:country>/<string:name>")
+def result(song_genre, country, name):
+    return render_template('result.html', title='Result', song_genre=song_genre, country=country, name=name)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,8 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class SearchForm(FlaskForm):
-    genre = StringField('genre', validators=[DataRequired()])
+    genre = StringField('Genre', validators=[DataRequired()])
     submit = SubmitField('GET LIT')
 
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired()])
+    email = StringField('Email',
+                        validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
